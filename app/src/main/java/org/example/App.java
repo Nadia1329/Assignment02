a155a2dc6d159962aca934ed098b33f0f5e67456
Nadia M <tm53929@mail.harpercollege.edu>
2026-01-20 13:13:55 -0800

    Deleted xyz

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class App {
-  public static void main(String[] args xyz) {
+  public static void main(String[] args ) {
     System.out.println("Write a driver program for the Converter class here!");
     System.out.println("You can delete these print statements and replace them with your code.");
   }

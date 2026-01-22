b4e1f1d1a95e0b05a0cd0c62b100d61b1e335ab3
Nadia M <tm53929@mail.harpercollege.edu>
2026-01-22 13:59:02 -0800

    Made proper code w/ conversions

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class App {
   public static void main(String[] args ) {
-    System.out.println("Write a driver program for the Converter class here!");
-    System.out.println("You can delete these print statements and replace them with your code.");
+    Converter converter = new Converter();
+    System.out.println("Welcome to the Terribly Weighty Subject!");
+    int ounces = 56;
+    System.out.println("Testing converter with " + ounces + " ounces.\n");
+    System.out.println(ounces + " oz is equivalent to " + converter.toPoundsAndOunces(ounces) + ".");
+    System.out.println(ounces + " oz is equivalent to " + converter.toPounds(ounces) + ".\n");
+    System.out.println("Thank you Goodbye!");
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,14 +1,21 @@
 package org.example;
 
 public class Converter {
-  // Write the code needed to make the tests in
-  // TestConverter.java pass. I have provided the
-  // tests for you in this lab to give you an example
-  // of how they should look. In the future, you will
-  // be responsible for writing your own tests.
+  public String toPounds(int ounces) {
+    double pounds = ounces / 16.0;
+    String formatted = String.format("%.4f", pounds);
+    if (pounds == 1.0)
+    {
+      return formatted + " lb";
+    } else {
+      return formatted + " lbs";
+    }
+  }
 
-  // After you have written the code to make the tests
-  // pass, exercise your code in a driver function in
-  // App.java. This shows that your business logic can
-  // run in a console program.
+public String toPoundsAndOunces(int ounces) {
+  int lbs = ounces / 16;
+  int oz = ounces % 16;
+  String lbUnit = (lbs == 1) ? "lb" : " lbs";
+  return lbs + " " + lbUnit + " " + oz + " oz";
+  }
 }
